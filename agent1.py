aa943362b4baaaf2471e32837aafcff2bcf285af
Samuel Beaubien <Samuel.Beaubien@Gmail.com>
2020-08-03 11:41:56 -0400

    Modify get_highest_link to be in-place, add comments

--- agent1.py
+++ agent1.py
@@ -1,13 +1,26 @@
 
+import gym
+
 from columnar import columnar
 
-class link:
+class Link:
     def __init__(self, start, finish, weight):
         self.start = start
         self.finish = finish
         self.weight = weight
 
-class node:
+    def update(self, update_value):
+        """Add (positive or negative) update value to the weight 
+
+        Args:
+            update_value (int): value to use for update
+        """
+        self.weight += update_value
+        # Make sure that weight is min 0
+        if (self.weight<0):
+            self.weight = 0
+
+class Node:
     
     def __init__(self, value, is_end_node):
         """
@@ -22,11 +35,15 @@ class node:
 
 
     def add_link(self, node, weight):
-        self.links.append(link(self, node, weight))
+        self.links.append(Link(self, node, weight))
 
     def get_highest_link(self):
-        self.links.sort(key=lambda x: x.weight, reverse = True)
-        return self.links[0]
+        """Get this node's link with the highest weight
+
+        Returns:
+            link: [description]
+        """
+        return max(self.links, key=lambda x: x.weight)
 
     def get_move(self):
         if (self.is_end_node == True):
@@ -38,45 +55,52 @@ class node:
     def is_dead(self):
         one_link_alive = False
         for link in self.links:
-            if (link.weight != 0):
+            if (link.weight > 0):
                 one_link_alive = True
         return not one_link_alive
 
-class network:
-    def __init__(self, input_nodes, output_nodes):
+class Network:
+    def __init__(self, input_nodes, output_nodes, modification_factor):
         self.input_nodes = input_nodes
         self.output_nodes = output_nodes
+        self.modification_factor = modification_factor
 
     def train_model(self, env, num_iter):
+        """Train the neural network to solve the "FrozenLake" puzzle
 
-        input_nodes = self.input_nodes
+        Args:
+            env (TimeLimit): OpenAI environment
+            num_iter (int): Maximum number of game that can be played
 
-        modification_factor = 2
-
+        Returns:
+            int: number of game played before the agent is able to succeed
+        """
         # Each iteration plays a full new game
         for i in range(num_iter):
             self.draw()
             observation = env.reset()
             # Each iteration pays a single move
             for j in range(100):
-                # Play
-                node = input_nodes[observation]
+                # Get move
+                node = self.input_nodes[observation]
                 link = node.get_highest_link()
                 output_node = link.finish
                 action = output_node.value
+                # Play
                 observation, reward, done, info = env.step(action)
                 # Feedback
+                last_node = self.input_nodes[observation]
                 if (done and j == 99):
                     break
                 if (done and int(reward) == 0):
-                    link.weight -= modification_factor
+                    link.update(-self.modification_factor)
                     break
                 if (done and int(reward) == 1):
                     print ("SUCCESS!")
                     return i
-                last_node = input_nodes[observation]
+                # Decrease weight if went to dead node
                 if (last_node.is_dead()):
-                    link.weight -= modification_factor
+                    link.weight -= self.modification_factor
                     break
 
     def draw(self):
@@ -91,37 +115,33 @@ class network:
         #Print table
         table = columnar(data, headers=None, no_borders=True)
         print (table)
-         
-            # print ("Node: " + str(node.value))
-            # for i, link in zip(range(0, len(node.links)), node.links):
-            #     print (f"   Link {i}: {link.weight}")
 
 
 def main():
-    # Input
+    # Create input nodes
     input_nodes = []
     for i in range(0, 16):
-        input_nodes.append(node(i, False))   
-    # Moves
+        input_nodes.append(Node(i, False))   
+    # Create output nodes
     output_nodes = []
     for i in range(0, 4):
-        output_nodes.append(node(i, True))
+        output_nodes.append(Node(i, True))
     
-    # Link nodes 
+    # Link input and output together
     default_weight = 10
     for start in input_nodes:
         for finish in output_nodes:
             start.add_link(finish, default_weight)
     
-    # Start environment
-    import gym
-    env = gym.make('FrozenLake-v0')
+    # Create wrapper network object
+    model = Network(input_nodes, output_nodes, 2)
 
-    model = network(input_nodes, output_nodes)
-    model.draw()
+    # Start environment
+    env = gym.make('FrozenLake-v0')
 
     # Train
     num_episodes = model.train_model(env, 1000)
+  
 
     print ("Solved in " + str(num_episodes) + " episodes.")
 
